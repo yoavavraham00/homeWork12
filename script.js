a634599b9ee50473e99ed3d633b85daadd7cda93
yoavavraham00 <114678073+yoavavraham00@users.noreply.github.com>
2023-02-16 15:56:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// fetch("https://go-apod.herokuapp.com/apod")
+//   .then((respons) => respons.json())
+//   .then((data) => {
+//     document.querySelector("h1").innerText = data.title;
+//     document.querySelector("img").src = data.url;
+
+//     document.querySelector("#D").innerText = data.date;
+//     document.querySelector("#C").innerText = data.copyright;
+//     document.querySelector("#E").innerText = data.explanation;
+
+//     if (currentData == null) {
+//       let object = {};
+//     }
+//     if (object[data.date] === undefined) {
+//       object[data.date] = data;
+//       localStorage.setItem("picturData", JSON.stringify(object));
+//     }
+//   });
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
